7d7b50f95854c17470f4f6761767c769d97e392a
Mampionona RAMAHAZOMANANA <50689325+Mampionona33@users.noreply.github.com>
2024-03-15 05:58:41 +0000

    feat: Add DocumentManager class with methods to add and retrieve documents

---
+++ DocumentManager.php
@@ -0,0 +1,22 @@
+class DocumentManager{
+    private $pdfStrings = array();
+    private $positions = array();
+    private $pageNumbers = array();
+    private $docIds = array();
+
+    public function addDocument($name, $string, $position, $pageNumber, $docId) {
+        $this->pdfStrings[$name] = $string;
+        $this->positions[$name] = $position;
+        $this->pageNumbers[$name] = $pageNumber;
+        $this->docIds[$name] = $docId;
+    }
+
+    public function getDocuments() {
+        return array(
+            'pdfStrings' => $this->pdfStrings,
+            'positions' => $this->positions,
+            'pageNumbers' => $this->pageNumbers,
+            'docIds' => $this->docIds
+        );
+    }
+}
