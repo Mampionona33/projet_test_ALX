3bdd2db6105a6c076c690cad01aa495c2027353b
Mampionona33 <ramamps33@gmail.com>
2024-03-25 21:09:46 +0300

    🖥️ wip: Refacotring send email

--- src/Controllers/EmailFormater/EmailFormater.php
+++ src/Controllers/EmailFormater/EmailFormater.php
@@ -32,20 +32,305 @@ class EmailFormater
      */
     private $devisDateRequest;
 
+    /**
+     * @var string|null
+     */
+    private $mandatStringAdministratifFinancier;
+
+    /**
+     * @var string|null
+     */
+    private $mandatStringAdministratif;
+
+
+    /**
+     * @var string|null
+     */
+    private $mandatStringFinancier;
+
+    /**
+     * @var string|null
+     */
+    private $attestationConsentementString;
+
+    /**
+     * @var string|null
+     */
+    private $numMpr2;
+
+    /**
+     * @var string|null
+     */
+    private $procurationString;
+
+    /**
+     * @var bool
+     */
+    private $isSubvention;
+
+    /**
+     * @var string|null
+     */
+    private $subventionString;
+
+    /**
+     * @var string|null
+     */
+    private $pdfDocumentMprString;
+
+    /**
+     * @var string|null
+     */
+    private $nomDocumentMpr;
+
+    /**
+     * @var string|null
+     */
+    private $amoString;
+
+    /**
+     * @var string|null
+     */
+    private $refusamoString;
+
+    /**
+     * @var string|null
+     */
+    private $mandatSpecialString;
+
+    /**
+     * @var string|null
+     */
+    private $mandat1Asc2String;
+
+
+    /**
+     * @var string|null
+     */
+    private $mandat2Asc2String;
+
+    /**
+     * @var string|null
+     */
+    private $nomSubvention;
+
+    /**
+     * @var string|null
+     */
+    private $lettreEngagementString;
+
+    /**
+     * @var string|null
+     */
+    private $tvaString;
+
+
+    /**
+     * @var string|null
+     */
+    private $ccString;
+
+    /**
+     * @var string|null
+     */
+    private $mandatSibel1String;
+
     public function __construct(string $sender_email, string $devisDateRequest)
     {
         $this->email = new PHPMailer();
         $this->sender_email = $sender_email;
         $this->devisDateRequest = $devisDateRequest;
+        $this->isSubvention = false;
         // -----------------------------
 
         $this->setupServer();
-        $this->setupAttachments();
     }
 
     /**
      * Setter and getter
      */
+    public function setMandatSibel1String(string|null $mandatSibel1String): void
+    {
+        $this->mandatSibel1String = $mandatSibel1String;
+    }
+    public function getMandatSibel1String(): string|null
+    {
+        return $this->mandatSibel1String;
+    }
+
+    public function setCcString(string|null $ccString): void
+    {
+        $this->ccString = $ccString;
+    }
+    public function getCcString(): string|null
+    {
+        return $this->ccString;
+    }
+
+    public function setTvaString(string|null $tvaString): void
+    {
+        $this->tvaString = $tvaString;
+    }
+    public function getTvaString(): string|null
+    {
+        return $this->tvaString;
+    }
+
+    public function setLettreEngagementString(string|null $lettreEngagementString): void
+    {
+        $this->lettreEngagementString = $lettreEngagementString;
+    }
+    public function getLettreEngagementString(): string|null
+    {
+        return $this->lettreEngagementString;
+    }
+
+    public function setNomSubvention(string|null $nomSubvention): void
+    {
+        $this->nomSubvention = $nomSubvention;
+    }
+    public function getNomSubvention(): string|null
+    {
+        return $this->nomSubvention;
+    }
+
+
+    public function setMandat2Asc2String(string|null $mandat2Asc2String): void
+    {
+        $this->mandat2Asc2String = $mandat2Asc2String;
+    }
+    public function getMandat2Asc2String(): string|null
+    {
+        return $this->mandat2Asc2String;
+    }
+
+    public function setMandat1Asc2String(string|null $mandat1Asc2String): void
+    {
+        $this->mandat1Asc2String = $mandat1Asc2String;
+    }
+    public function getMandat1Asc2String(): string|null
+    {
+        return $this->mandat1Asc2String;
+    }
+
+    public function setMandatSpecialString(string|null $mandatSpecialString): void
+    {
+        $this->mandatSpecialString = $mandatSpecialString;
+    }
+    public function getMandatSpecialString(): string|null
+    {
+        return $this->mandatSpecialString;
+    }
+
+    public function setRefusamoString(string|null $refusamoString): void
+    {
+        $this->refusamoString = $refusamoString;
+    }
+    public function getRefusamoString(): string|null
+    {
+        return $this->refusamoString;
+    }
+
+    public function setAmoString(string|null $amoString): void
+    {
+        $this->amoString = $amoString;
+    }
+    public function getAmoString(): string|null
+    {
+        return $this->amoString;
+    }
+
+    public function setNomDocumentMpr(string|null $nomDocumentMpr): void
+    {
+        $this->nomDocumentMpr = $nomDocumentMpr;
+    }
+    public function getNomDocumentMpr(): string|null
+    {
+        return $this->nomDocumentMpr;
+    }
+
+    public function setPdfDocumentMprString(string|null $pdfDocumentMprString): void
+    {
+        $this->pdfDocumentMprString = $pdfDocumentMprString;
+    }
+    public function getPdfDocumentMprString(): string|null
+    {
+        return  $this->pdfDocumentMprString;
+    }
+
+    public function setSubventionString(string|null $subventionString): void
+    {
+        $this->subventionString = $subventionString;
+    }
+    public function getSubventionString(): string|null
+    {
+        return $this->subventionString;
+    }
+
+    public function setIsSubvention(bool $isSubvention): void
+    {
+        $this->isSubvention = $isSubvention;
+    }
+    public function getIsSubvention(): bool
+    {
+        return $this->isSubvention;
+    }
+
+    public function setProcurationString(string|null $procurationString): void
+    {
+        $this->procurationString = $procurationString;
+    }
+    public function getProcurationString(): string|null
+    {
+        return $this->procurationString;
+    }
+
+
+    public function setNumMpr2(): string|null
+    {
+        return $this->numMpr2;
+    }
+    public function getNumMpr2(): string|null
+    {
+        return $this->numMpr2;
+    }
+
+    public function setAttestationConsentementString(string $attestationConsentementString): void
+    {
+        $this->attestationConsentementString = $attestationConsentementString;
+    }
+    public function getAttestationConsentementString(): string|null
+    {
+        return $this->attestationConsentementString;
+    }
+
+    public function setMandatStringFinancier(string $mandatStringFinancier): void
+    {
+        $this->mandatStringFinancier = $mandatStringFinancier;
+    }
+    public function getMandatStringFinancier(): string|null
+    {
+        return $this->mandatStringFinancier;
+    }
+
+    public function setMandatStringAdministratif(string $mandatStringAdministratif): void
+    {
+        $this->mandatStringAdministratif = $mandatStringAdministratif;
+    }
+    public function getMandatStringAdministratif(): string|null
+    {
+        return $this->mandatStringAdministratif;
+    }
+
+    public function setMandatStringAdministratifFinancier(string $mandatStringAdministratifFinancier): void
+    {
+        $this->mandatStringAdministratifFinancier = $mandatStringAdministratifFinancier;
+    }
+    public function getMandatStringAdministratifFinancier(): string|null
+    {
+        return $this->mandatStringAdministratifFinancier;
+    }
+
     public function setDevisDateRequest(string $devisDateRequest): void
     {
         $this->devisDateRequest = $devisDateRequest;
@@ -54,6 +339,7 @@ class EmailFormater
     {
         return $this->devisDateRequest;
     }
+
     public function setSenderName(string $senderName): void
     {
         $this->senderName = $senderName;
@@ -112,6 +398,47 @@ class EmailFormater
     }
 
     /**
+     * Setup attachments
+     */
+    public function setupAttachments(): void
+    {
+        $devisDate = DateTime::createFromFormat("Y-m-d H:i:s", $this->devisDateRequest);
+        $cutoffDate = DateTime::createFromFormat("d/m/Y H:i:s", "01/05/2010 00:00:00");
+
+        if ($devisDate < $cutoffDate) {
+            $this->attachMandatIfPresent($this->mandatStringAdministratifFinancier, 'mandat-administratif-financier.pdf');
+            $this->attachMandatIfPresent($this->mandatStringAdministratif, 'mandat-administratif.pdf');
+            $this->attachMandatIfPresent($this->mandatStringFinancier, 'mandat-financier.pdf');
+        } elseif ($this->numMpr2 !== null) {
+            $this->attachMandatIfPresent($this->attestationConsentementString, 'Attestation_de_consentement.pdf');
+            $this->attachMandatIfPresent($this->mandatStringAdministratifFinancier, 'mandat-administratif-financier.pdf');
+            $this->attachMandatIfPresent($this->mandatStringAdministratif, 'mandat-administratif.pdf');
+            $this->attachMandatIfPresent($this->mandatStringFinancier, 'mandat-financier.pdf');
+        }
+
+        if ($this->isSubvention) {
+            $this->attachProcuringMandatIfPathContains('ghe', $this->subventionString ?? '', 'lettre_fond_solidarite.pdf');
+            $this->attachProcuringMandatIfPathContains('doovision', $this->subventionString ?? '', 'confirmation-des-aides.pdf');
+            $this->attachProcuringMandatIfPathContains('efe', $this->subventionString ?? '', 'lettre_confirmation_devis.pdf');
+            $this->attachSubventionIfPathDoesNotContain(['ghe', 'doovision', 'efe']);
+        }
+
+        $this->attachProcuringMandatIfPathContains('futurenv', $this->procurationString ?? '', 'Procuration.pdf');
+        $this->attachMandatIfPresent($this->pdfDocumentMprString, $this->nomDocumentMpr . '.pdf');
+        $this->attachMandatIfPresent($this->amoString, 'contrat_amo.pdf');
+        $this->attachMandatIfPresent($this->refusamoString, 'refus_amo.pdf');
+        $this->attachMandatIfPresent($this->mandatSpecialString, 'mandat_special.pdf');
+        $this->attachMandatIfPresent($this->mandat1Asc2String, 'mandat_representation.pdf');
+        $this->attachMandatIfPresent($this->mandat2Asc2String, 'mandat_special.pdf');
+        $this->attachMandatIfPresent($this->lettreEngagementString, 'lettre_engagement.pdf');
+        $this->attachMandatIfPresent($this->tvaString, 'attestation_tva.pdf');
+        $this->attachMandatIfPresent($this->ccString, 'cadre_contribution.pdf');
+
+
+        $this->attachProcuringMandatIfPathContains('leader-energie', $this->mandatSibel1String ?? '', 'Mandat_Gestionnaire_de_reseau.pdf');
+    }
+
+    /**
      * Attach mandat if present
      * @param string|null $mandatString
      * @param string|null $fileName
@@ -123,20 +450,67 @@ class EmailFormater
         if ($mandatString !== null && $fileName !== null) {
             $this->email->addStringAttachment($mandatString, $fileName);
         }
-        if ($mandatString === null && $fileName === null) {
-            throw new \Exception('No mandat attached');
+        if ($mandatString === null) {
+            throw new \Exception('No mandat string');
+        }
+        if ($fileName === null) {
+            throw new \Exception('No mandat file name');
+        }
+    }
+
+    private function pathExist(): bool
+    {
+        return $this->path !== null;
+    }
+
+    private function noPathThrowerException(): void
+    {
+        if (!$this->pathExist()) {
+            throw new \Exception('No path');
         }
     }
 
     /**
-     * Setup attachments
+     * Attach procuring mandat if path contains word
+     *
+     * @param string $fileName Le nom du fichier à rechercher dans le chemin
+     * @param string $mandatString La chaîne représentant le mandat à attacher
+     * @param string $attachmentName Le nom de la pièce jointe à attacher
+     * 
+     * @return void
+     * 
+     * @throws \Exception Si le chemin n'est pas défini ou si le nom du fichier ou de la pièce jointe est vide
      */
-    private function setupAttachments(): void
+    private function attachProcuringMandatIfPathContains(string $fileName, string $mandatString = '', string $attachmentName = ''): void
     {
-        $devisDate = DateTime::createFromFormat("Y-m-d H:i:s", $this->devisDateRequest);
-        $cutoffDate = DateTime::createFromFormat("d/m/Y H:i:s", "01/05/2010 00:00:00");
+        $this->noPathThrowerException();
 
-        if ($devisDate < $cutoffDate) {
+        if (empty($mandatString) || empty($attachmentName)) {
+            throw new \Exception('Le nom du fichier ou le nom de la pièce jointe ne peut pas être vide.');
+        }
+
+        if (strpos($this->path, $fileName) !== false) {
+            $this->attachMandatIfPresent($mandatString, $attachmentName);
+        }
+    }
+
+    /**
+     * Undocumented function
+     * @param array<string> $fileName
+     * @return void
+     */
+    private function attachSubventionIfPathDoesNotContain(array $fileName): void
+    {
+        $found = false;
+        foreach ($fileName as $key => $value) {
+            if (strpos($this->path, $key) !== false) {
+                $found = true;
+                break;
+            }
+        }
+
+        if (!$found && $this->subventionString !== null && $this->nomSubvention !== null) {
+            $this->email->addStringAttachment($this->subventionString, $this->nomSubvention . '.pdf');
         }
     }
 }
